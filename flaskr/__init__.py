3dcb27d6f093cad62a6851117abb991b4c89e6c1
贾孝新 <xiaoxin.jia@longleding.com>
2019-11-08 13:00:12 +0800

    update auth

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -8,9 +8,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
+        DEBUG=True,
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
@@ -31,5 +31,8 @@ def create_app(test_config=None):
 
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
